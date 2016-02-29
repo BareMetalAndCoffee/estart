28c2915895229acf7e7b7dcf8e39fe0a3be663f4
Dean Sutton <d.n.sutton@gmail.com>
2016-02-29 22:33:50 +0000

    Adding makefile and cmake helpers.

--- makefile
+++ makefile
@@ -1,7 +1,8 @@
-MAKEFLAGS:=--silent
+MAKEFLAGS=--silent
 
-BUILD_DIR:=$(CURDIR)/bld
-SOURCE_DIR:=$(CURDIR)/src
+ROOT_DIR=$(CURDIR)
+BUILD_DIR=$(ROOT_DIR)/bld
+SOURCE_DIR=$(ROOT_DIR)/src
 
 all: clean build test
 
@@ -12,14 +13,6 @@ all: clean build test
 clean:
 	rm -rf $(BUILD_DIR)
 
-$(BUILD_DIR)/Makefile:
-	mkdir -p $(BUILD_DIR)
-	cd $(BUILD_DIR) && cmake \
-       -G "Unix Makefiles" \
-       $(SOURCE_DIR)
-
-build: $(BUILD_DIR)/Makefile
-	$(MAKE) -C $(BUILD_DIR)
-
-test: build
-	$(MAKE) -C $(BUILD_DIR) test
+include $(SOURCE_DIR)/lib/make/generic_build_targets.mk
+$(eval $(call GENERIC_BUILD_TGT,linux-x86-gcc))
+$(eval $(call GENERIC_BUILD_TGT,linux-x86-clang))
---
+++ src/lib/make/generic_build_targets.mk
@@ -0,0 +1,33 @@
+define GENERIC_BUILD_TGT
+
+# Define new build targets
+.PHONY: clean_$(1)
+.PHONY: build_$(1)
+.PHONY: test_$(1)
+
+$(BUILD_DIR)/$(1)/Makefile:
+	mkdir -p $(BUILD_DIR)/$(1)
+	cd $(BUILD_DIR)/$(1) && cmake \
+      -D CMAKE_TOOLCHAIN_FILE=$(SOURCE_DIR)/lib/cmake/toolchains/$(1).cmake \
+      -G "Unix Makefiles" \
+      $(SOURCE_DIR)
+
+clean_$(1): $(BUILD_DIR)/$(1)/Makefile
+	$(MAKE) -C $(BUILD_DIR)/$(1) clean
+
+_build_info_$(1):
+	printf "\033[0;32mBUILD INFORMATION\n"
+	printf "TOOLCHAIN: $(1)\n\033[0m"
+
+build_$(1): _build_info_$(1) $(BUILD_DIR)/$(1)/Makefile
+	$(MAKE) -C $(BUILD_DIR)/$(1)
+
+
+test_$(1): build_$(1)
+	$(MAKE) -C $(BUILD_DIR)/$(1) test
+
+# Link build targets to parent targets
+build: build_$(1)
+test: test_$(1)
+
+endef
